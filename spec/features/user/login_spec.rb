dbc01131d831e5f5ae3cf3c59059377e555a4bd3
Festus kiambi <kiamfestoh@gmail.com>
2018-03-27 21:57:42 +0300

    Add a feature spec to test if a user can go to a single post from the home page

--- spec/features/user/login_spec.rb
+++ spec/features/user/login_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature "login", :type => :feature do
     scenario 'user navigates to the login page and successfully logs in',js: true do
         user
         visit root_path
-        find('nav a',text: 'login').click
+        find('nav a',text: 'Login').click
         fill_in 'user[email]',  with: user.email
         fill_in 'user[password]', with: user.password
         find('.login-button').click
